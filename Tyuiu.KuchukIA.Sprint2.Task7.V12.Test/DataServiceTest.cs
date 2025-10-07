97eff7f977c020ac91e08c0330f060f04d1c7d0d
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-10-07 20:02:21 +0500

    Task7

---
+++ Tyuiu.KuchukIA.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KuchukIA.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.KuchukIA.Sprint2.Task7.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea1()
+        {
+            DataService ds = new DataService();
+
+            double x = 1;
+            double y = 0.5;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KuchukIA.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Tyuiu.KuchukIA.Sprint2.Task7.V12.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: И.А. Кучук | ПИНб-25-1";
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* Спринт #2                                                                                   *");
+        Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                                                   *");
+        Console.WriteLine("* Задание #7                                                                                  *");
+        Console.WriteLine("* Вариант #12                                                                                 *");
+        Console.WriteLine("* Выполнил: Кучук Иван Александрович |  ПИНб-25-1                                             *");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                                    *");
+        Console.WriteLine("* Написать программу на C#, которая запрашивает исходные данные (вещественные значения)       *");
+        Console.WriteLine("* и вычисляет, находится ли точка с координатами X,Y в заштрихованной области.                *");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                            *");
+        Console.WriteLine("***********************************************************************************************");
+
+
+        Console.WriteLine("Введите значение X:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine("Введите значение Y:");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+
+        Console.WriteLine();
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                  *");
+        Console.WriteLine("***********************************************************************************************");
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка НЕ находится в заштрихованной области");
+        }
+
+        Console.ReadKey();
+
+    }
+}
