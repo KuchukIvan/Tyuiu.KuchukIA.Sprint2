04859409fb4cef9c9b856e09cb5e3f170bd4c064
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-10-14 18:54:56 +0500

    Task7

--- Tyuiu.KuchukIA.Sprint2.Task7.V12.Lib/DataService.cs
+++ Tyuiu.KuchukIA.Sprint2.Task7.V12.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KuchukIA.Sprint2.Task7.V12.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if (((x >= 0) && (x <= 2) && (y >= 0) && (y <= 1) && (y <= 2 - x) && (y <= Math.Pow(x, 2))) || ((y <= -2) && (y >= 4) && (y >= 2 - x) && (y <= Math.Pow(x, y))))
+            if (((x >= 0) && (x <= 2) && (y >= 0) && (y <= 1) && (y <= 2 - x) && (y <= Math.Pow(x, 2))) || ((x <= -2) && (y >= 4) && (y >= 2 - x) && (y <= Math.Pow(x, y))))
             {
                 res = true;
             }
